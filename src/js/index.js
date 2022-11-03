3e40c749f84f10753b46cdde324e56b51080130c
art5551 <afmrp@protonmail.com>
2022-11-03 04:59:54 -0500

    Added checkbox to javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Adding eventListener to the slider to update the pagevews and the cost per month
-  document.querySelector('.slider input').addEventListener('change',fetch_value);
+  document.querySelector('.slider input').addEventListener('input',fetch_value);
+  //Update price when the discount toggle is changed
+  document.getElementById('check').addEventListener('click', fetch_value);
 
 });
 
-// No improvements necssary - done!
-function fetch_value(e) {
-  const slider = e.target;
-  console.log(slider.value)
+
+function fetch_value() {
+  //Get the slider elemeny to fetch value of views
+  //Had to different functions but not worth it - just get slider here
+  // const slider = e.target;
+  const slider = document.querySelector('.slider input');
+  // console.log(slider.value)
+  //Keep the progrss bbar in sync with the slider
   const progress_bar = document.querySelector('.slider progress');
   progress_bar.value = slider.value;
-  console.log(slider_value);
+
+  // console.log(slider_value);
+  //Next two so it can update the innerhtml
   const slider_value = document.querySelector('.slider-value');
   const amount = document.querySelector('.pricing_cost');
   
   
 
-  const cost = calc_cost(slider.value);
+  var cost = calc_cost(slider.value);
+  var add_discount = document.getElementById('check').checked;
+  console.log(add_discount);
+  if (add_discount) {
+    const discount_price = (cost * .76)
+    cost = discount_price.toFixed(2);
+  }
+
   amount.innerHTML = '$' + cost;
   slider_value.innerHTML = slider.value;
 }
